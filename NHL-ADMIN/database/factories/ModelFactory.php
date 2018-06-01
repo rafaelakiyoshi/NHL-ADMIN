4c0972a73de543f00bfef159a3c625d7ec386a44
Rafael Akiyoshi <rafah94@hotmail.com>
2018-06-01 14:11:39 -0300

    Implementing random data for games result with model factory

--- NHL-ADMIN/database/factories/ModelFactory.php
+++ NHL-ADMIN/database/factories/ModelFactory.php
@@ -11,9 +11,20 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+// $factory->define(App\User::class, function (Faker\Generator $faker) {
+//     return [
+//         'name' => $faker->name,
+//         'email' => $faker->email,
+//     ];
+// });
+
+$factory->define(App\Game::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'id_team1' => rand(1, 12),
+        'id_team2' => rand(1, 12),
+        'score_team1' => rand(1, 10),
+        'score_team2' => rand(1, 10),
+        'game_date' => "".rand(1, 30)."062018",
+        'watched' => rand(500, 10000),
     ];
 });
--- NHL-ADMIN/database/seeds/DatabaseSeeder.php
+++ NHL-ADMIN/database/seeds/DatabaseSeeder.php
@@ -18,79 +18,49 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
         DB::table('teams')->delete();
         DB::table('games')->delete();
-        $teams = array(
-            [
-                'name' => 'Carolina Hurricanes', 
-                'logo' => 'https://goo.gl/qBrkyG', 
-                'division' => 'Metropolitan',
-                'wins' => 5,
-                'loses' => 2,
-            ],
-            [
-                'name' => 'Winnipeg Jets', 
-                'logo' => 'https://goo.gl/bXzdoM', 
-                'division' => 'Central',
-                'wins' => 6,
-                'loses' => 1,
-            ],
-            [
-                'name' => 'Boston Bruins', 
-                'logo' => 'https://goo.gl/1nX3NX', 
-                'division' => 'Atlantic',
-                'wins' => 4,
-                'loses' => 3,
-            ],
-            [
-                'name' => 'Anaheim Ducks', 
-                'logo' => 'https://goo.gl/qV1TfC', 
-                'division' => 'Pacific',
-                'wins' => 3,
-                'loses' => 4,
-            ],
-        );
+        
+        $path = realpath(__DIR__ . '/../../database/seeds/TeamsSeed.json');
+        $teams = json_decode(file_get_contents($path), true);
 
-        $games = array(
-            [
-                'id_team1' => 1, 
-                'id_team2' => 2, 
-                'score_team1' => 2,
-                'score_team2' => 5,
-                'game_date' => '06022017',
-                'watched' => 2000,
-            ],
-            [
-                'id_team1' => 2, 
-                'id_team2' => 3, 
-                'score_team1' => 4,
-                'score_team2' => 5,
-                'game_date' => '08022017',
-                'watched' => 3500,
-            ],
-            [
-                'id_team1' => 3, 
-                'id_team2' => 4, 
-                'score_team1' => 1,
-                'score_team2' => 5,
-                'game_date' => '06032017',
-                'watched' => 2000,
-            ],
-            [
-                'id_team1' => 4, 
-                'id_team2' => 1, 
-                'score_team1' => 8,
-                'score_team2' => 5,
-                'game_date' => '09022017',
-                'watched' => 4000,
-            ],
-        );
+        $games = factory(App\Game::class, 100)->create();
+        // $games = array(
+        //     [
+        //         'id_team1' => 1, 
+        //         'id_team2' => 2, 
+        //         'score_team1' => 2,
+        //         'score_team2' => 5,
+        //         'game_date' => '06022017',
+        //         'watched' => 2000,
+        //     ],
+        //     [
+        //         'id_team1' => 2, 
+        //         'id_team2' => 3, 
+        //         'score_team1' => 4,
+        //         'score_team2' => 5,
+        //         'game_date' => '08022017',
+        //         'watched' => 3500,
+        //     ],
+        //     [
+        //         'id_team1' => 3, 
+        //         'id_team2' => 4, 
+        //         'score_team1' => 1,
+        //         'score_team2' => 5,
+        //         'game_date' => '06032017',
+        //         'watched' => 2000,
+        //     ],
+        //     [
+        //         'id_team1' => 4, 
+        //         'id_team2' => 1, 
+        //         'score_team1' => 8,
+        //         'score_team2' => 5,
+        //         'game_date' => '09022017',
+        //         'watched' => 4000,
+        //     ],
+        // );
 
         // Loop through each user above and create the record for them in the database
         foreach ($teams as $team) {
             Team::create($team);
-        }
-
-        foreach ($games as $game) {
-            Game::create($game);
         }
         Model::reguard();
     }
