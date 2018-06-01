b8bc6a3dceb24c28018c2572cb8db39229b70fc9
Rafael Akiyoshi <rafah94@hotmail.com>
2018-06-01 00:15:00 -0300

    Creating Seeder for Teams database

--- NHL-ADMIN/database/seeds/DatabaseSeeder.php
+++ NHL-ADMIN/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use App\Team;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        Model::unguard();
+        DB::table('teams')->delete();
+        $teams = array(
+            [
+                'name' => 'Carolina Hurricanes', 
+                'logo' => 'https://goo.gl/qBrkyG', 
+                'division' => 'Metropolitan',
+                'wins' => 5,
+                'loses' => 2,
+            ],
+            [
+                'name' => 'Winnipeg Jets', 
+                'logo' => 'https://goo.gl/bXzdoM', 
+                'division' => 'Central',
+                'wins' => 6,
+                'loses' => 1,
+            ],
+            [
+                'name' => 'Boston Bruins', 
+                'logo' => 'https://goo.gl/1nX3NX', 
+                'division' => 'Atlantic',
+                'wins' => 4,
+                'loses' => 3,
+            ],
+            [
+                'name' => 'Anaheim Ducks', 
+                'logo' => 'https://goo.gl/qV1TfC', 
+                'division' => 'Pacific',
+                'wins' => 3,
+                'loses' => 4,
+            ],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($teams as $team) {
+            Team::create($team);
+        }
+        Model::reguard();
     }
 }
