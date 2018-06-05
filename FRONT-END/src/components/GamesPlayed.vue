048144b2236886ca859ccf483d34b4d13e4d512b
Rafael Akiyoshi <rafah94@hotmail.com>
2018-06-05 03:15:23 -0300

    Adding graphic of games played

--- FRONT-END/src/components/GamesPlayed.vue
+++ FRONT-END/src/components/GamesPlayed.vue
@@ -14,63 +14,76 @@ export default {
     return {
       games: [],
       option: {
-    title : {
-        text: 'Games Played',
-        subtext: 'Wins vs Losses',
-        x:'center'
-    },
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['Wins','Losses','Draws']
-    },
-    series : [
-        {
+        title: {
+          text: 'Games Played',
+          subtext: 'Wins vs Losses',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: ['Wins', 'Losses', 'Draws']
+        },
+        series: [
+          {
             name: 'Games',
             type: 'pie',
-            radius : '55%',
+            radius: '55%',
             center: ['50%', '60%'],
-            data:[
-                {value:335, name:'Wins'},
-                {value:310, name:'Losses'},
-                {value:234, name:'Draws'},
+            data: [
+              { value: 335, name: 'Wins' },
+              { value: 310, name: 'Losses' },
+              { value: 234, name: 'Draws' }
             ],
             itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
             }
-        }
-    ]
-}
+          }
+        ]
+      }
     }
   },
   mounted () {
-    this.$http.get(`http://nhl.admin/api/teamGames/${this.$store.getters.getTeamDetail.id}`).then(response => {
-        // get body data
+    this.$http
+      .get(
+        `http://nhl.admin/api/teamGames/${this.$store.getters.getTeamDetail.id}`
+      )
+      .then(
+        response => {
+          // get body data
           this.games = response.body
           let wins = 0
           let losses = 0
           let draws = 0
           for (var x in this.games) {
-            if (this.games[x].id_team1 === this.$store.getters.getTeamDetail.id){
-              if (this.games[x].score_team1 > this.games[x].score_team2){
+            if (
+              this.games[x].id_team1 === this.$store.getters.getTeamDetail.id
+            ) {
+              if (this.games[x].score_team1 > this.games[x].score_team2) {
                 wins++
-              } else if (this.games[x].score_team1 < this.games[x].score_team2){
+              } else if (
+                this.games[x].score_team1 < this.games[x].score_team2
+              ) {
                 losses++
               } else {
                 draws++
               }
-            } else if (this.games[x].id_team2 === this.$store.getters.getTeamDetail.id){
-              if (this.games[x].score_team2 > this.games[x].score_team1){
+            } else if (
+              this.games[x].id_team2 === this.$store.getters.getTeamDetail.id
+            ) {
+              if (this.games[x].score_team2 > this.games[x].score_team1) {
                 wins++
-              } else if (this.games[x].score_team2 < this.games[x].score_team1){
+              } else if (
+                this.games[x].score_team2 < this.games[x].score_team1
+              ) {
                 losses++
               } else {
                 draws++
@@ -82,13 +95,14 @@ export default {
           this.option.series[0].data[2].value = draws
           this.myChart = echarts.init(document.getElementById('main'))
           this.myChart.setOption(this.option)
-        }, response => {
-        // error callback
-        })
+        },
+        response => {
+          // error callback
+        }
+      )
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
